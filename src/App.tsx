55a108897546993bfca5e4107b6a4d4ba216c76c
manuelbamise <etobi1930@gmail.com>
2026-06-26 16:53:51 +0100

    refactor(components):moved list logic from App.tsx to externalList component

--- src/App.tsx
+++ src/App.tsx
@@ -8,67 +8,9 @@ const App = () => {
   //   return x + y;
   // };
 
-  function alertObjNAme(name: string) {
-    alert(name);
-  }
-
-  interface obj {
-    id: number;
-    name: string;
-    age: number;
-    condition: boolean;
-  }
-
-  const firstObject: obj = {
-    id: 1,
-    name: "Ejhirooghene",
-    age: 37,
-    condition: true,
-  };
-
-  const SecondObject: obj = {
-    id: 2,
-    name: "ayoola",
-    age: 45,
-    condition: false,
-  };
-
-  const thirdObject: obj = {
-    id: 3,
-    name: "damian",
-    age: 30,
-    condition: true,
-  };
-
-  const newArray: obj[] = [firstObject, SecondObject, thirdObject];
-
   return (
     <>
       <h1>New Html file</h1>
-      {newArray &&
-        newArray.map((ob) => {
-          return (
-            <ol
-              key={ob.id}
-              style={{
-                borderRadius: "20px",
-                background: "red",
-                color: "white",
-                padding: "10px",
-              }}
-            >
-              <li>{ob.name}</li>
-              <li>{ob.age}</li>
-              <button
-                onClick={() => alertObjNAme(ob.name)}
-                disabled={!ob.condition}
-              >
-                Click ME for surprise
-              </button>
-            </ol>
-          );
-        })}
-
       <ExternalList />
     </>
   );
--- src/components/externalList.tsx
+++ src/components/externalList.tsx
@@ -1,7 +1,78 @@
+import userObject from "./userObject.json";
+
 const ExternalList = () => {
+  function changeInColor(e: React.MouseEvent<HTMLButtonElement>) {
+    e.currentTarget.style.backgroundColor = "lightgray";
+  }
+
+  function changeOutColor(e: React.MouseEvent<HTMLButtonElement>) {
+    e.currentTarget.style.backgroundColor = "white";
+  }
+
+  function alertObjNAme(name: string) {
+    alert(name);
+  }
+
+  interface obj {
+    id: number;
+    name: string;
+    age: number;
+    condition: boolean;
+  }
+
+  const newArray: obj[] = userObject;
+
   return (
     <>
-      <p>Hello from the external component</p>
+      <p>Hello from the external component</p> <br />
+      <div
+        style={{
+          margin: "auto",
+          display: "flex",
+          width: "50%",
+          padding: "20px",
+          backgroundColor: "gray",
+          color: "black",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {" "}
+        {newArray &&
+          newArray.map((ob) => {
+            return (
+              <ol
+                key={ob.id}
+                style={{
+                  width: "30%",
+                  listStylePosition: "inside",
+                  borderRadius: "10px",
+                  background: "white",
+                  padding: "10px",
+                }}
+              >
+                <li>{ob.name}</li>
+                <li>{ob.age}</li>
+                <button
+                  style={{
+                    color: "black",
+                    marginTop: "10px",
+
+                    padding: "10px",
+                    borderRadius: "5px",
+                  }}
+                  onMouseEnter={(e) => changeInColor(e)}
+                  onMouseLeave={(e) => changeOutColor(e)}
+                  onClick={() => alertObjNAme(ob.name)}
+                  disabled={!ob.condition}
+                >
+                  Click ME for surprise
+                </button>
+              </ol>
+            );
+          })}{" "}
+      </div>
     </>
   );
 };
