d5ee9a3f422662afa80f68ea4995c4cd1aef0b52
manuelbamise <etobi1930@gmail.com>
2026-06-26 16:30:47 +0100

    feat(components):added the new external list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import ExternalList from "./components/externalList";
+
 const App = () => {
   // const age = 18;
   // const ageLimit = 18;
@@ -6,21 +8,68 @@ const App = () => {
   //   return x + y;
   // };
 
-  interface object1 {
-    name: string;
-    age: number;
+  function alertObjNAme(name: string) {
+    alert(name);
   }
 
-  const firstObject: object1 = {
+  interface obj {
+    id: number;
+    name: string;
+    age: number;
+    condition: boolean;
+  }
+
+  const firstObject: obj = {
+    id: 1,
     name: "Ejhirooghene",
     age: 37,
+    condition: true,
   };
+
+  const SecondObject: obj = {
+    id: 2,
+    name: "ayoola",
+    age: 45,
+    condition: false,
+  };
+
+  const thirdObject: obj = {
+    id: 3,
+    name: "damian",
+    age: 30,
+    condition: true,
+  };
+
+  const newArray: obj[] = [firstObject, SecondObject, thirdObject];
 
   return (
     <>
       <h1>New Html file</h1>
-      <p>{firstObject.name}</p>
-      <p>{firstObject.age}</p>
+      {newArray &&
+        newArray.map((ob) => {
+          return (
+            <ol
+              key={ob.id}
+              style={{
+                borderRadius: "20px",
+                background: "red",
+                color: "white",
+                padding: "10px",
+              }}
+            >
+              <li>{ob.name}</li>
+              <li>{ob.age}</li>
+              <button
+                onClick={() => alertObjNAme(ob.name)}
+                disabled={!ob.condition}
+              >
+                Click ME for surprise
+              </button>
+            </ol>
+          );
+        })}
+
+      <ExternalList />
     </>
   );
 };
---
+++ src/components/externalList.tsx
@@ -0,0 +1,9 @@
+const ExternalList = () => {
+  return (
+    <>
+      <p>Hello from the external component</p>
+    </>
+  );
+};
+
+export default ExternalList;
